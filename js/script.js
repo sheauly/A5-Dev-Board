b69bd2d903ba4f47edb461d607f0e9a45465193d
halimawebd <inthesumon1010@gmail.com>
2025-03-01 22:51:33 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").addEventListener('click', function(){
+    const cheakButton = getCheakButtonById("task-ber");
+    
+    for (let i = 0; i < cheakButton.length; i--){
+        console.log(cheakButton);
+    }
+})
+
+
+
+ 
---
+++ utilities/utilities.js
@@ -0,0 +1,5 @@
+function getCheakButtonById(id) {
+    const cheakButton = document.getElementById(id).innerText;
+    const covertButton = parseInt(cheakButton);
+    return covertButton;
+}
